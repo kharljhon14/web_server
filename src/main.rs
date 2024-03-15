debe1bdb367ef6fdc6c04c51fb48b7cb73ec3409
kharle14 <enriquezkharl14@gmail.com>
2024-03-15 21:05:57 +0800

    Structs

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,24 @@ use std::fs;
 use std::io::Write;
 use std::sync::Mutex;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
+
 fn main() {}
